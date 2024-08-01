6855911e45a145e176bab6ae3bf04f13aeb20ce4
Manvendra Raj Singh <manvendrarajsinghmanny@gmail.com>
2024-08-01 13:51:36 +0530

    Adding chat-bot functinalities

---
+++ chat-bot/htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://elearning-adbi.org/wp-content/uploads/2021/02/governing-sustainable-finance-feature-image-1080x675-1.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://assets.bizclikmedia.net/900/e23691c264cba78a235de0bd05c5ed6b:0f8ab506787efd73c6120aed770d9f3f/gettyimages-1297492947.webp">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
